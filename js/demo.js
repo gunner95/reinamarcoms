9eec2715eba66414d644c86214e7eed3aaec824a
gunner95 <prabansal@microsoft.com>
2021-02-06 18:55:00 +0530

    Added new section

--- js/demo.js
+++ js/demo.js
@@ -36,7 +36,7 @@ var leftNav = function() {
             x.innerHTML = `
             <header class="header fixed-top header-animated">
               <nav class="navbar navbar-expand-lg navbar-light py-3">
-                <div class="container"><a class="navbar-brand" href="index.html"><img src="img/custom/logo2.jpg" alt="..." width="150"></a>
+                <div class="container"><a class="navbar-brand" href="index.html"><img src="img/custom/logo2.jpg" alt="..." width="100"></a>
                   <button class="navbar-toggler navbar-toggler-right bg-color-grey" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
